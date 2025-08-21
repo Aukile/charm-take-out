3ebd7c243248fd960c4c620d95e91266601d3f20
Ankrya <342987434@qq.com>
2025-08-21 09:21:29 +0800

    start

---
+++ charm-take-out-backend/src/main/java/com/anx/takeout/CharmTakeOutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anx.takeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharmTakeOutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharmTakeOutBackendApplication.class, args);
+	}
+
+}
---
+++ charm-take-out-backend/src/test/java/com/anx/charmtakeoutbackend/CharmTakeOutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anx.charmtakeoutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharmTakeOutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
